4ce2233814d5a66d64ca5628b7f896ed38113d3f
veljkoilic12 <veljkoilic5@gmail.com>
2022-10-25 19:53:37 +0200

    add envelope for our json response; switch json.Marshal for json.MarshalIndent to have prettier json response in curl

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,15 +6,17 @@ import (
 
 // declare a handler which writes a json response with information about the app status, env and ver
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a map which holds the information that we want to send in the response.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	// Declare an envelope map containing the data for response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	// Use our writeJSON helper to write the JSON response
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Print(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
