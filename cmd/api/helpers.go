039be4e4d48c79607efb733d58f4fd192a60864f
veljkoilic12 <veljkoilic5@gmail.com>
2022-10-25 19:42:23 +0200

    create and annotate movie struct; rewrite showmovieHandler

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -49,5 +49,7 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data any, h
 	// Add the "Content-Type: application/json" header, then write the status code and JSON response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
+	w.Write(js)
+
 	return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"greenlight.veljkoilic.net/internal/data"
 	"net/http"
+	"time"
 )
 
 // "POST /v1/movies" endpoint
@@ -18,5 +20,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of the movie%d\n", id)
+	// Create a new instance of the Movie struct, containing the ID we extracted from the URL
+	// and some dummy data.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
