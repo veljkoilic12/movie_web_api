888fe8ba321540c614a8326443d825897977e782
veljkoilic12 <veljkoilic5@gmail.com>
2022-10-26 17:48:10 +0200

    add json decoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"greenlight.veljkoilic.net/internal/data"
 	"net/http"
@@ -9,7 +10,26 @@ import (
 
 // "POST /v1/movies" endpoint
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie...")
+	// Declare an anonymous struct to hold the information that we expect to be in the HTTP request
+	// body; this struct will be our target decode destination.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// Initialize a new json.Decoder instance which reads from the request body and then use Decode()
+	// to decode the body contents into the input struct. Notice that we pass a *pointer* to the input
+	// when we call Decode().
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Dump the contents of the input struct in the HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // "GET /v1/movies" endpoint
